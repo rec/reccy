510ff15f31595b7d490502b2e25cea49885bb7c4
Tom Ritchford <tom@swirly.com>
2026-09-07 14:59:44 +0200

    Require Python 3.13

--- reccy/services/spec.py
+++ reccy/services/spec.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
+import tomllib
 from pathlib import Path
-
-import tomli
 
 from . import models
 
 
 def load(path: Path) -> models.ServiceSpec:
     with path.open('rb') as file:
-        return models.ServiceSpec.model_validate(tomli.load(file))
+        return models.ServiceSpec.model_validate(tomllib.load(file))
